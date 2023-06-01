1f31f75c61e5ee5dcac88d45df933a2fb7562a0c
basic <basic@LAPTOP-5MS9HNOH>
2023-06-01 21:17:44 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //Console.Write("barev erkir");
+            //Console.WriteLine("barev erkir");
+            //byte t1 = 254;
+            //sbyte t2 = 125;
+            //int t3 = t1 + t2;
+            //sbyte sb1 = 125;
+            //sbyte sb3 = 100;
+            //Console.WriteLine(t3);
+            //Console.WriteLine("--------------");
+            //int y = 454645;
+            //int yy = 45613;
+            //float ff = 65457f;
+            //double dd = 4577846;
+            //decimal nn = 657845m;
+
+            //string t = "";
+            //string kk = "hgfh";
+            //string ss = t + kk;
+            //Console.WriteLine(ss);
+
+
+            //char c = 'c';
+            //char l = 'l';
+            ////int o = 'c' + 'l';
+            ////Console.WriteLine('o');
+
+
+            int c = 66;
+            int d = 1000;
+            if (c > d)
+            {
+                Console.WriteLine("C > d-ic");
+            }
+            else
+            {
+                Console.WriteLine("d-n > c-ic");
+            }
+        }
+
+
+        Console.Write("X= ");
+            string y = Console.ReadLine();
+        Console.write("Y= ");
+            string y1 = Console.ReadLine();
+        Console.write("Sum= "+ y + y1);
+        
+
+    }
+}
